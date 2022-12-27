5b7da9ca2146ead1fe36e8ef695a6c2279255532
Gokul1902 <121498782+Gokul1902@users.noreply.github.com>
2022-12-27 18:59:08 +0530

    Add files via upload

---
+++ World/src/main/java/Time/TimeShower.java
@@ -0,0 +1,42 @@
+package Time;
+
+import java.awt.image.RescaleOp;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+
+
+public class TimeShower extends HttpServlet {
+	public void service(ServletRequest req,ServletResponse res) throws IOException{
+		try {
+			
+		
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		String country = req.getParameter("country");
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		sdf.setTimeZone(TimeZone.getTimeZone(country));
+		sdf.applyPattern("dd MMM yyyy HH:mm:ss z");
+		RequestDispatcher rDispatcher = req.getRequestDispatcher("/TimeShow.html");
+		rDispatcher.include(req, res);
+		out.println("<h1>"+country+"--------"+sdf.format(Calendar.getInstance().getTime())+"</h1>");
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+		
+	
+
+}
